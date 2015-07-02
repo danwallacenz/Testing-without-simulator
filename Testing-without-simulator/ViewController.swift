7924daa3eab40b47e94b1de0996c983c434dbbe5
Dan Wallace <daniel.wallace@ikegps.com>
2015-07-02 12:36:04 +1200

    Initial Commit

---
+++ Testing-without-simulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing-without-simulator
+//
+//  Created by Daniel Wallace on 2/07/15.
+//  Copyright (c) 2015 Daniel Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
